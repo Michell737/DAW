761b22dfff6f3d9f476ade6758e8682fd821afe6
Michell <A01700396@itesm.mx>
2021-08-31 20:34:34 -0500

    comandos de consola

---
+++ Lab 05/js.js
@@ -0,0 +1,23 @@
+//Uso de js
+
+//Uso de la consola
+console.log("hola");
+console.info("Estamos en el bloque de DAW-BD");
+console.warn("Advertencia");
+console.error("Error!!!!");
+
+//Variables, constantes y alcance
+
+//Estructuras de control
+
+//alerts, prompts y confirms, escribir en el html
+
+//Funciones
+
+//Funciones flecha
+
+//Arreglos
+
+//Recorrido de arreglos
+
+//Objetos
