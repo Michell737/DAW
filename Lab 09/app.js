0cca8b2d67022696c08041f4e4a3d5a70a78f107
Michell <A01700396@itesm.mx>
2021-09-06 03:57:00 -0500

    guardar

--- Lab 09/app.js
+++ Lab 09/app.js
@@ -1,15 +1,39 @@
-//Primer prograa con node
+//Primer programa con node
 
 console.log("Hola mundo!!");
 console.info("Estamos usando js en una pc");
 
 let x=5;
-console.log(x);
+//console.log(x);
 
 const y=x+4;
-console.log(y);
+//console.log(y);
 
 //fs es el prototipo de js que trabaja con el sistema de archivos. fs significa file system.
 const file_system = require('fs');
 
 file_system.writeFileSync("comida.txt","gorditas");
+
+//Código asíncrono
+
+const arreglo = [5000,3,4,300,2000,1500,200,34,12,400,100]
+
+for(let item of arreglo){
+    setTimeout( () =>{
+        console.log(item);
+    },item);
+}
+
+//Servidor web
+const http = require('http');
+
+const server = http.createServer( (request, response) => {
+    console.log(request.url);
+    response.setHeader('Content-Type','text/html');
+    response.write('<h1>Hola mundo! Esta pagina no existe</h1>');
+    response.statusCode = 404;
+    response.end();
+});
+
+//Dice a que puerto escucho
+server.listen(3000);
